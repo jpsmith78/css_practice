6885abf5b3ea8d0cb1c33f797ec308a17cbbe493
Justin Smith <justinpetersmith@gmail.com>
2019-08-15 12:08:35 -0400

    added drag and drop function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const drag = (event) => {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+const allowDrop = (event) => {
+  event.preventDefault();
+}
+
+const drop = (event) => {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+}
